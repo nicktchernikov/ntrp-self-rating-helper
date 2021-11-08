c56a74f061feec0149ddc264f0e3af543943b9f7
Nick T <nick.tchernik@gmail.com>
2021-11-07 21:13:52 -0800

    working 3rd approach

--- testV2.js
+++ testV2.js
@@ -1,3 +1,8 @@
+/* Better plan: 
+    Fetch questions once onload
+    Filter questions every type typeIndex changes
+*/
+
 const categoryAnswers = {
     'groundstrokes' : [],
     'return of serve' : [],
@@ -13,42 +18,50 @@ const types = [
     'serve'
 ];
 
+let questions = [];
+
 const currentScreen = 'General';
 
-const fetchQuestions = async () => {
-    const response = await fetch('./questions.json');
-    const questions = await response.json();
-    return questions;
+const fetchAllQuestions = async () => {
+    return (await fetch('./questions.json')).json();
 }
 
 const fetchQuestionsOfType = async (type) => {
-    const questions = await fetchQuestions();
-    const questionsOfType = questions.filter(q => q.type === type);
-    console.log(questionsOfType);
+    let questions = await fetchAllQuestions();
+    const questionsOfType = questions.filter(q => q.type.toLowerCase() === type.toLowerCase());
+    return await questionsOfType;
 }
 
 const TestV2 = () => {
-
     const [typeIndex, setTypeIndex] = React.useState(0);
-    const [currentType, setCurrentType] = React.useState(types[typeIndex]);
+    const [questionsOfType, setQuestionsOfType] = React.useState([]);
 
     const handleButtonClick = () => {
+        console.log('>>>> In handleButtonClick');
         setTypeIndex(typeIndex + 1);
-        setCurrentType(types[typeIndex]);
     }
 
-    React.useEffect(() => {
-        console.log('>>>> In useEffect()');
-        const questionsOfType = fetchQuestionsOfType(currentType);
-        console.log(questionsOfType);
-    }, []);
+    // React.useEffect(async () => {
+    //    questions = await fetchAllQuestions();
+    // }, []);
+
+    React.useEffect(async () => { // when typeIndex changes
+        console.log('>>>> in typeIndex useEffect');
+        console.log('>>>> typeIndex', typeIndex);
+        console.log('>>>> types[typeIndex]', types[typeIndex]);
+        fetchQuestionsOfType(types[typeIndex]).then((data) => {
+            console.log('>>>> data', data);
+            setQuestionsOfType(data);
+        });
+    }, [typeIndex]);
 
     return(
         <div>
             {currentScreen === 'General' &&
                 <div>
                     
-                    <input onClick={() => handleButtonClick()} type='button' />
+                    <input onClick={() => handleButtonClick()} type='button'
+                    value='Click' />
 
                 </div>
             }
---
+++ testV3.js
@@ -0,0 +1,59 @@
+const RatingHelper = () => {
+    const [allQuestions, setAllQuestions] = React.useState([]);
+    const [typeIndex, setTypeIndex] = React.useState(0);
+
+    const types = [
+        'general', 
+        'groundstrokes', 
+        'net play', 
+        'return of serve',
+        'serve'
+    ];
+
+    const handleClick = () => {
+        console.log('Click');
+        setTypeIndex(typeIndex + 1);
+    }
+
+    const fetchAllQuestions = async () => {
+        return (await fetch('./questions.json')).json();
+    }
+    
+    const filterQuestionsByType = (allQuestions, currentType) => {
+        return allQuestions.filter(question => question.type.toLowerCase() === currentType.toLowerCase());
+    }
+
+    React.useEffect(async () => {
+        fetchAllQuestions().then((questionData) => setAllQuestions(questionData));
+    }, []);
+
+    const displayQuestion = (question) => {
+        return (
+            <label className='question-box' key={question.id} htmlFor={question.id}>
+                <input
+                    id={question.id}
+                    name='question'
+                    value='question.id'
+                    type='radio' 
+                    className='question-radio' 
+                />
+                    {question.text}
+            </label>
+        );
+    }
+ 
+    const questions = filterQuestionsByType(allQuestions, types[typeIndex]);
+
+    return (
+        <div> 
+            <div className='title'>Rating Helper</div>
+            <hr />
+            <div className='questions'>
+                {questions.map(question => displayQuestion(question))}
+            </div>
+            <input type='button' value='Click' onClick= {() => handleClick()} />
+        </div>
+    );
+}
+
+ReactDOM.render(<RatingHelper />, document.querySelector('#root'));
